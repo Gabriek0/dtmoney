999b5cdda09c0bcf04da403d931d8d4f7f0900ae
Gabriek0 <gabriel.sanches170@gmail.com>
2021-11-10 22:01:55 -0300

    consumindo api com miragejs e adicionando modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+    openNewTransactionModal: () => void;
+}
+
+export function Header({ openNewTransactionModal }: HeaderProps) {
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button">
+                <button onClick={openNewTransactionModal} type="button">
                     Nova transação
                 </button>
             </Content>
