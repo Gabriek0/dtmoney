944988f49f8eecf86388a49a68606649cf1ece07
Gabriek0 <gabriel.sanches170@gmail.com>
2021-11-18 21:22:34 -0300

    estilizando cores do internas dos botoes do modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from "react-modal"
-import { Content } from "./styles"
+import { Content, RadioBox, TransactionTypeContainer } from "./styles"
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
+import { useState } from "react"
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -8,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const [type, setType] = useState('deposit');
+
     return (
         <Modal
             isOpen={isOpen}
@@ -29,6 +34,30 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 <input placeholder="Título" />
 
                 <input placeholder="Valor" />
+
+                <TransactionTypeContainer>
+                    <RadioBox
+                        type="button"
+                        onClick={() => { setType('deposit') }}
+                        isActive={type === 'deposit'}
+                        activeColor='green'
+
+                    >
+                        <img src={incomeImg} alt="income img" />
+                        <span>Entradas</span>
+                    </RadioBox>
+
+                    <RadioBox
+                        type="button"
+                        onClick={() => { setType('withdraw') }}
+                        isActive={type === 'withdraw'}
+                        activeColor='red'
+                    >
+                        <img src={outcomeImg} alt="outcome img" />
+                        <span>Saídas</span>
+                    </RadioBox>
+                </TransactionTypeContainer>
+
 
                 <input placeholder="Categoria" />
 
